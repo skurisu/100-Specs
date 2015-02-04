85f09db49be38b7fc0d96177937a1c62de231b05
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-02-03 23:16:23 -1000

    adds solarSystem class and add/remove planet methods

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -579,7 +579,17 @@ Garden.prototype.grow = function () {
  *   removePlanet
  *
  */
+function SolarSystem (){
+  this.planets = [];
+}
 
+SolarSystem.prototype.addPlanet = function (planet){
+  this.planets.push(planet);
+};
+
+SolarSystem.prototype.removePlanet = function (planet) {
+  this.planets.splice(this.planets.indexOf(planet), 1);
+};
 
 /* Step 33
  *
