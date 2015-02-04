ad537d2184ea0a5772bf2458aa400ddd4de95bb7
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-02-04 11:48:25 -1000

    adds 2 instances of Door

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1090,8 +1090,8 @@ var catBox = new Box(cat,true);
 var christmasPresent = new Box("something",false);
 
 // Create 2 doors
-var automaticDoor;
-var bankVault;
+var automaticDoor = new Door(true);
+var bankVault = new Door(false);
 
 // Create 2 shoes
 var rubySlippers;
