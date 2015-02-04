c69e4d4cf0d59a583933a87c40bb8d75cb18c6e9
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-02-04 11:39:17 -1000

    adds 2 new instances of Shape

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1081,8 +1081,8 @@ var civic = new Vehicle("Honda","Civic");
 var forte = new Vehicle("KIA","Forte");
 
 // Create 2 shapes with different numbers of sides
-var square;
-var hexagon;
+var square = new Shape(4);
+var hexagon = new Shape(6);
 
 // Create 2 boxes
 var catBox;
